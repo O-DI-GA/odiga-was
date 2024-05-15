a0399a9505eb8797ad2c76a75945cb25e7e9d722
kimnoca <kimnoca@naver.com>
2024-05-15 18:35:11 +0900

    feat : user 엔티티 추가

---
+++ src/main/java/yu/cse/odiga/global/type/Role.java
@@ -0,0 +1,4 @@
+package yu.cse.odiga.global.type;
+
+public enum Role {
+}
---
+++ src/main/java/yu/cse/odiga/user/domain/User.java
@@ -0,0 +1,39 @@
+package yu.cse.odiga.user.domain;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import yu.cse.odiga.global.type.Role;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique = true, updatable = false, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
