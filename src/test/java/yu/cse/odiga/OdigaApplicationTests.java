2313fa1e6ec35a6fc9f29ffdd70f300e52c2448f
kimnoca <kimnoca@naver.com>
2024-05-15 18:03:17 +0900

    feat : init project

---
+++ src/test/java/yu/cse/odiga/OdigaApplicationTests.java
@@ -0,0 +1,13 @@
+package yu.cse.odiga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdigaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
