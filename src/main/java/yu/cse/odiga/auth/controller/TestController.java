bbd74898bf24a2f8eb854cfddcbf97b297ca4ca0
kimnoca <kimnoca@naver.com>
2024-05-16 15:20:05 +0900

    feat : 인증 TestController.java 생성

---
+++ src/main/java/yu/cse/odiga/auth/controller/TestController.java
@@ -0,0 +1,18 @@
+package yu.cse.odiga.auth.controller;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/")
+public class TestController {
+
+    @GetMapping("test")
+    public String test() {
+        return "test";
+    }
+}
