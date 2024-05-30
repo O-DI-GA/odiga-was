bad95391baf80be174a26a28d55581cf07903999
kimnoca <kimnoca@naver.com>
2024-05-30 21:08:31 +0900

    chore : review 관련 TODO 추가

--- src/main/java/yu/cse/odiga/store/domain/Review.java
+++ src/main/java/yu/cse/odiga/store/domain/Review.java
@@ -14,12 +14,16 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Review {
+
+    // TODO : User entity 랑 연관 맺어야함, score 0 ~ 5 점 enum 으로 바꾸는게 좋을듯 (정수)
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String content;
     private double rating;
     private String imageUrl;
+
     private String userNickname;
     private String userProfileImageUrl;
     private LocalDateTime createDate;
