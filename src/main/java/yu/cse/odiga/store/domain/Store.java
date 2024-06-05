9749e6c0ac5d4b61e15f8cbc0af594b4c106ba63
jahyun <jahyungoo6979@gmail.com>
2024-06-06 00:15:46 +0900

    chore : long 자료형 Long으로 수정

--- src/main/java/yu/cse/odiga/store/domain/Store.java
+++ src/main/java/yu/cse/odiga/store/domain/Store.java
@@ -18,7 +18,7 @@ import java.util.List;
 public class Store {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     @Column
     private String storeName;
