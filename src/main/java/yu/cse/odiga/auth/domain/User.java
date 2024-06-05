687397eb8cceed06750617a9bc9703042b48ea1a
jahyun <jahyungoo6979@gmail.com>
2024-06-05 23:58:49 +0900

    chore : Review와 User 클래스 관계 삭제

--- src/main/java/yu/cse/odiga/auth/domain/User.java
+++ src/main/java/yu/cse/odiga/auth/domain/User.java
@@ -48,7 +48,4 @@ public class User {
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<LikeStore> likeStores;
-
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Review> reviews = new ArrayList<>();
 }
