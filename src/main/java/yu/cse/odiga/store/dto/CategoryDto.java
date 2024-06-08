44176fca9aa034f9a9f1c9d1cb54ab8574d59755
kimnoca <kimnoca@naver.com>
2024-06-08 19:03:59 +0900

    Merge : Category 기능 Merge

---
+++ src/main/java/yu/cse/odiga/store/dto/CategoryDto.java
@@ -0,0 +1,14 @@
+package yu.cse.odiga.store.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryDto {
+    private String name;
+}
