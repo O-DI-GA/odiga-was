47eb32aa6006908efc08cab339ffedec2730cbb5
kimnoca <kimnoca@naver.com>
2024-06-21 11:12:59 +0900

    feat : 웨이팅 취소 상태 구현 및 웨이팅 취소 오류 해결

--- src/main/java/yu/cse/odiga/waiting/type/WaitingStatus.java
+++ src/main/java/yu/cse/odiga/waiting/type/WaitingStatus.java
@@ -3,5 +3,5 @@ package yu.cse.odiga.waiting.type;
 public enum WaitingStatus {
     // TODO : CANCEL 상태 추가 하기
     // CANCEL : 완료 상태가 아닌 취소 상태를 의마흐는 waiting status
-    COMPLETE, INCOMPLETE
+    COMPLETE, INCOMPLETE, CANCEL
 }
